2b4c2ab17195a8c0544edf84b6bb715e1b168256
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-03-04 06:27:17 +0530

    Day 13 tasks: Update & Delete.

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -9,3 +9,13 @@ export const createTask = async (taskData) => {
     const response = await API.post("/tasks", taskData);
     return response.data;
 };
+
+export const updateTask = async (id, updatedDta) => {
+    const response = await API.put(`/tasks/${id}`,updatedDta);
+    return response.data;
+}
+
+export const deleteTask = async (id) => {
+    const response = await API.delete(`/tasks/${id}`)
+    return response.data;
+}
