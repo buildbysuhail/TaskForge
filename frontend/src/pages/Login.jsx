8d3372c2c3e72af3ade7546f6dbf1c634658037b
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-03-09 11:36:17 +0530

    some radix-ui packages installed, Login.jsx: UI polished based on shadcn

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,18 @@
 import { useState } from "react";
 import API from "../services/api";
 import { useNavigate } from "react-router-dom";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
 console.log("API URL:", API);
 function Login() {
@@ -44,22 +56,68 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login Page</h2>
+    // <div>
+    //   <h2>Login Page</h2>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+    //   {error && <p style={{ color: "red" }}>{error}</p>}
 
-      <form action="" onSubmit={handleSubmit}>
-        <input name="email" placeholder="Enter Email" onChange={handleChange} />
-        <input
-          name="password"
-          type="password"
-          placeholder="Enter Password"
-          onChange={handleChange}
-        />
-        <button type="submit">{loading ? "Logging in..." : "Login"}</button>
-      </form>
-    </div>
+    //   <form action="" onSubmit={handleSubmit}>
+    //     <input name="email" placeholder="Enter Email" onChange={handleChange} />
+    //     <input
+    //       name="password"
+    //       type="password"
+    //       placeholder="Enter Password"
+    //       onChange={handleChange}
+    //     />
+    //     <button type="submit">{loading ? "Logging in..." : "Login"}</button>
+    //   </form>
+    // </div>
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+        <CardAction>
+          <Button variant="link">Sign Up</Button>--
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
 
