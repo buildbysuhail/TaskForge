fde045fbbb3729e46614991af838942077083e73
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-09-17 10:48:04 +0530

    ui/button.jsx > Slot.Root => Slot

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -47,7 +47,8 @@ function Button({
   asChild = false,
   ...props
 }) {
-  const Comp = asChild ? Slot.Root : "button"
+  // const Comp = asChild ? Slot.Root : "button"
+  const Comp = asChild ? Slot : "button"
 
   return (
     <Comp
