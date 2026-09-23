9e742b7d886ad1a46c9b8d10a175e3e9c2241dca
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-09-23 23:07:42 +0530

    dark:bg-zinc-700

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -91,7 +91,7 @@ function Dashboard() {
                 options={tabItems.map((t) => ({ value: t.value, label: t.label, icon: t.icon }))}
                 className="w-[180px]" // removed mx-auto
                 valueClassName="text-center w-full font-bold"
-                contentClassName="font-medium w-[180px] bg-stone-200"
+                contentClassName="font-medium w-[180px] bg-stone-200 dark:bg-zinc-700"
                 triggerClassName="h-9 py-[17px]"
                 align="start"
                 sideOffset={4}
